c4113df04591308b667993b635bbb21f40ba7b3c
Coast415 <jonas.fandee@gmail.com>
2016-11-14 16:40:35 +0100

    Starting
    Hurry the fk up

---
+++ Digit Catcher/src/digitCasher/DigitCasher.java
@@ -0,0 +1,10 @@
+package digitCasher;
+
+public class DigitCasher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Digit Catcher/src/digitCasher/Digitcathaer.java
@@ -0,0 +1,10 @@
+package digitCasher;
+
+public class Digitcathaer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
